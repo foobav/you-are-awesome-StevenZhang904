7c7a2bcaa3380977bd9bcdbf54914564dd08cc8f
foobav <104128111+foobav@users.noreply.github.com>
2021-08-03 10:07:24 +0000

    changes.

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,5 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
 #
-# This configuration file is loaded before any de
+# This configuration file is loaded before any dependency and
+# is restricted to this proj
