c3569c347606abf07f4cef926c7caed56a6f6237
foobav <104128111+foobav@users.noreply.github.com>
2021-08-19 16:36:37 +0000

    crypto: msm: Update clock vote for improved performance

--- config/config.exs
+++ config/config.exs
@@ -15,4 +15,4 @@ config :study_reminder, StudyReminderWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "+jkIoDa33F3mJA8lmVPCMT4pUpnSrJ6H2mzD9CZDZWe/DFZWUOjGlqaSIeepMma2",
   render_errors: [view: StudyReminderWeb.ErrorView, accepts: ~w(json)],
-  pubsub
+  pubsub: [name: StudyReminder.PubSub, adap
