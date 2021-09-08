5b1462558f96319e577e217999025057ca9199e0
foobav <104128111+foobav@users.noreply.github.com>
2021-09-08 09:11:09 +0000

    added app.js

--- config/config.exs
+++ config/config.exs
@@ -19,4 +19,7 @@ config :study_reminder, StudyReminderWeb.Endpoint,
 
 # Configures Elixir's Logger
 config :logger, :console,
-  format: "$time $metadata[$level] $message
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# U
