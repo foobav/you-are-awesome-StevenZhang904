cbbeb1abc2714a34611710149cf1c086baab4dec
foobav <104128111+foobav@users.noreply.github.com>
2021-09-09 18:53:16 +0000

    1.8.4

--- config/config.exs
+++ config/config.exs
@@ -26,4 +26,5 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configurat
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.
