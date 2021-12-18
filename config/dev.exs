fd3244cfd0b383cf7bf5e789d6354c86958d943a
foobav <104128111+foobav@users.noreply.github.com>
2021-12-18 19:29:23 +0000

    crop image

--- config/dev.exs
+++ config/dev.exs
@@ -39,4 +39,7 @@ config :study_reminder, StudyReminderWeb.Endpoint,
 #       port: 4001,
 #       cipher_suite: :strong,
 #       keyfile: "priv/cert/selfsigned_key.pem",
-#       certfile: "priv/cert/selfsigned.pe
+#       certfile: "priv/cert/selfsigned.pem"
+#     ],
+#
+# If desired, both `http:` and
