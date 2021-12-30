9f61b3d1f01ca2452e2c3c0dc08d9dade8d83925
foobav <104128111+foobav@users.noreply.github.com>
2021-12-30 21:37:40 +0000

    v1.6.4

--- config/dev.exs
+++ config/dev.exs
@@ -53,4 +53,5 @@ config :logger, :console, format: "[$level] $message\n"
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Initialize plugs at runtime for faster development co
+# Initialize plugs at runtime for faster development compilation
+config :phoenix, :plug_init_mode, :runti
