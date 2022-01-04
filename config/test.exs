5fa24a931e568faea1af9ba2d89c50c1b14a0e86
foobav <104128111+foobav@users.noreply.github.com>
2022-01-04 05:47:44 +0000

    Removed build files

--- config/test.exs
+++ config/test.exs
@@ -14,4 +14,5 @@ config :study_reminder, StudyReminderWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and er
+# Print only warnings and errors during test
+config :logger, level: :wa
