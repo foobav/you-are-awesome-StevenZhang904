b9fa095877ad72ffd5f8b1b9f218e54d06622c26
foobav <104128111+foobav@users.noreply.github.com>
2022-02-25 20:18:52 +0000

    New translations Resources.resx (Polish)

--- lib/study_reminder_web/authentication/current_user.ex
+++ lib/study_reminder_web/authentication/current_user.ex
@@ -2,4 +2,6 @@ defmodule StudyReminderWeb.Authentication.CurrentUser do
     @moduledoc """
     Load user from database by token and put on connection assigns
     """
-    def init(_opts), d
+    def init(_opts), do: :ok
+  
+    def call(conn, _opts) d
