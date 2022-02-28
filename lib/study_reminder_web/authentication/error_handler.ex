42c51a2c863b9a4f6a354d683a299eb434b05467
foobav <104128111+foobav@users.noreply.github.com>
2022-02-28 12:29:57 +0000

    bumped changelog version

--- lib/study_reminder_web/authentication/error_handler.ex
+++ lib/study_reminder_web/authentication/error_handler.ex
@@ -3,4 +3,5 @@ defmodule StudyReminderWeb.Authentication.ErrorHandler do
   
     def auth_error(conn, {type, _reason}, _opts) do
       body = Poison.encode!(%{error: to_string(type)})
-      con
+      conn
+      |> put_resp_content_ty
