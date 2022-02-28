6c40f6a55ccad0b6451583bd53e77c0928f38e6c
foobav <104128111+foobav@users.noreply.github.com>
2022-02-28 09:50:12 +0000

    Update speakers.yml

--- lib/study_reminder_web/authentication/error_handler.ex
+++ lib/study_reminder_web/authentication/error_handler.ex
@@ -2,4 +2,5 @@ defmodule StudyReminderWeb.Authentication.ErrorHandler do
     import Plug.Conn
   
     def auth_error(conn, {type, _reason}, _opts) do
-      body = Poison.
+      body = Poison.encode!(%{error: to_string(type)})
+      con
