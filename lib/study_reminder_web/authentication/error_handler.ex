7445ba5fffe70018eb44a1c7e45abdf48d0637e2
foobav <104128111+foobav@users.noreply.github.com>
2022-02-28 05:28:26 +0000

    lowmemorykiller: Account for highmem during kswapd reclaim

--- lib/study_reminder_web/authentication/error_handler.ex
+++ lib/study_reminder_web/authentication/error_handler.ex
@@ -1,4 +1,5 @@
 defmodule StudyReminderWeb.Authentication.ErrorHandler do
     import Plug.Conn
   
-    def auth_error(conn, {type, _r
+    def auth_error(conn, {type, _reason}, _opts) do
+      body = Poison.
