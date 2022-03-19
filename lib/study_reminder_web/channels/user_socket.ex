981c2275eb0c71adb414e70cc0a50812db50ac2b
foobav <104128111+foobav@users.noreply.github.com>
2022-03-19 16:28:38 +0000

    docs updated

--- lib/study_reminder_web/channels/user_socket.ex
+++ lib/study_reminder_web/channels/user_socket.ex
@@ -6,4 +6,5 @@ defmodule StudyReminderWeb.UserSocket do
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
-  # verification, you can put default assigns
+  # verification, you can put default assigns into
+  # the socket that will be set for all cha
