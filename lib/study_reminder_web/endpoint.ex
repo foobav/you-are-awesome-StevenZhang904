a4b87aa5587be93bb3cb92f32d2462d53b01e39c
foobav <104128111+foobav@users.noreply.github.com>
2022-04-09 16:33:45 +0000

    OSE header fixes

--- lib/study_reminder_web/endpoint.ex
+++ lib/study_reminder_web/endpoint.ex
@@ -8,4 +8,6 @@ defmodule StudyReminderWeb.Endpoint do
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
-  # when deploying your static file
+  # when deploying your static files in production.
+  plug Plug.Static,
+  
