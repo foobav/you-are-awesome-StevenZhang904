d434d71d2c5424f4f8f6c15668778585cedfaf51
foobav <104128111+foobav@users.noreply.github.com>
2022-04-13 20:18:11 +0000

    Regenerate gemspec for version 0.1.5

--- lib/study_reminder_web/endpoint.ex
+++ lib/study_reminder_web/endpoint.ex
@@ -33,4 +33,5 @@ defmodule StudyReminderWeb.Endpoint do
   plug Plug.Head
 
   # The session will be stored in the cookie and signed,
-  # this means its contents can be read
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt
