9265d60e52c8bd06fd0434ac4490f48e6dea8438
foobav <104128111+foobav@users.noreply.github.com>
2022-04-14 08:43:41 +0000

    update pot

--- lib/study_reminder_web/endpoint.ex
+++ lib/study_reminder_web/endpoint.ex
@@ -34,4 +34,5 @@ defmodule StudyReminderWeb.Endpoint do
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
-  # Set :encryption_salt
+  # Set :encryption_salt if you would also like to encrypt it.
+  plug Plug.Sess
