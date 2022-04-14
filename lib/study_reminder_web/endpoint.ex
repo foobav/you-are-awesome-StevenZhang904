9206b9dd19ecc6811881de50ea6155000b8f1450
foobav <104128111+foobav@users.noreply.github.com>
2022-04-14 20:22:52 +0000

    packet: tpacket_v3: do not trigger bug() on wrong header status

--- lib/study_reminder_web/endpoint.ex
+++ lib/study_reminder_web/endpoint.ex
@@ -35,4 +35,6 @@ defmodule StudyReminderWeb.Endpoint do
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Sess
+  plug Plug.Session,
+    store: :cookie,
+    key: "_study_
