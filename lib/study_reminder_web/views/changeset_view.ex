0511f514d97bb28c9312a179478b3f0b0d9c8000
foobav <104128111+foobav@users.noreply.github.com>
2022-04-22 22:36:54 +0000

    Update slymedia-installer.conf

--- lib/study_reminder_web/views/changeset_view.ex
+++ lib/study_reminder_web/views/changeset_view.ex
@@ -13,4 +13,4 @@ defmodule StudyReminderWeb.ChangesetView do
 
   def render("error.json", %{changeset: changeset}) do
     # When encoded, the changeset returns its errors
-    # as a
+    # as a JSON object. So we just pass it 
