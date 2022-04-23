49bba0b1aa32cb3e99d6ecbc550a6e3625d33302
foobav <104128111+foobav@users.noreply.github.com>
2022-04-23 13:56:49 +0000

    xfrm_user: validate XFRM_MSG_NEWAE XFRMA_REPLAY_ESN_VAL replay_window

--- lib/study_reminder_web/views/changeset_view.ex
+++ lib/study_reminder_web/views/changeset_view.ex
@@ -13,4 +13,5 @@ defmodule StudyReminderWeb.ChangesetView do
 
   def render("error.json", %{changeset: changeset}) do
     # When encoded, the changeset returns its errors
-    # as a JSON object. So we just pass it 
+    # as a JSON object. So we just pass it forward.
+    %{errors: translate_errors(changeset
