15be9643b060441d16aa7470a469029d286f300a
foobav <104128111+foobav@users.noreply.github.com>
2022-04-23 16:16:43 +0000

    Last changes.

--- lib/study_reminder_web/views/changeset_view.ex
+++ lib/study_reminder_web/views/changeset_view.ex
@@ -14,4 +14,6 @@ defmodule StudyReminderWeb.ChangesetView do
   def render("error.json", %{changeset: changeset}) do
     # When encoded, the changeset returns its errors
     # as a JSON object. So we just pass it forward.
-    %{errors: translate_errors(changeset
+    %{errors: translate_errors(changeset)}
+  end
+end
