1917132b5cdb4fca7e9842966b319a838e006ffb
foobav <104128111+foobav@users.noreply.github.com>
2022-04-24 14:38:20 +0000

    zram: make compression algorithm selection possible

--- lib/study_reminder_web/views/error_view.ex
+++ lib/study_reminder_web/views/error_view.ex
@@ -2,4 +2,6 @@ defmodule StudyReminderWeb.ErrorView do
   use StudyReminderWeb, :view
 
   # If you want to customize a particular status code
-  # for a certain format, you may uncomment b
+  # for a certain format, you may uncomment below.
+  # def render("500.json", _assigns) do
+  
