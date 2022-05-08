b3740293ed6c6b68624ee2bef1184a746db110f5
foobav <104128111+foobav@users.noreply.github.com>
2022-05-08 06:30:51 +0000

    all

--- lib/study_reminder_web.ex
+++ lib/study_reminder_web.ex
@@ -3,4 +3,6 @@ defmodule StudyReminderWeb do
   The entrypoint for defining your web interface, such
   as controllers, views, channels and so on.
 
-  This can be
+  This can be used in your application as:
+
+      use St
