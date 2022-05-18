22d19c1bca1b6f007bf4f5129a6a871afd6259c4
foobav <104128111+foobav@users.noreply.github.com>
2022-05-18 16:13:27 +0000

    paginator

--- lib/study_reminder_web.ex
+++ lib/study_reminder_web.ex
@@ -36,4 +36,5 @@ defmodule StudyReminderWeb do
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
 
-      import StudyReminde
+      import StudyReminderWeb.ErrorHelpers
+      import Stu
