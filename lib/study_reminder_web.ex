e3dd5e2b3fb8ec80f54c3ffe949a65007f101890
foobav <104128111+foobav@users.noreply.github.com>
2022-05-20 18:21:46 +0000

    New translations p02_ch02_the_first_test_rape.md (Spanish, Guatemala)

--- lib/study_reminder_web.ex
+++ lib/study_reminder_web.ex
@@ -60,4 +60,5 @@ defmodule StudyReminderWeb do
   @doc """
   When used, dispatch to the appropriate controller/view/etc.
   """
-  defmacro __using__(which) when is_ato
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, wh
