ba5ad5fd98a23438c9a76141ccbbc4290b4256cf
foobav <104128111+foobav@users.noreply.github.com>
2022-09-23 20:29:49 +0000

    modified readme

--- priv/repo/migrations/20191123224450_add_reminders_to_users.exs
+++ priv/repo/migrations/20191123224450_add_reminders_to_users.exs
@@ -3,4 +3,7 @@ defmodule StudyReminder.Repo.Migrations.AddRemindersToUsers do
 
   def change do
     alter table("reminders") do
-      add :user_id, re
+      add :user_id, references(:users)
+    end
+  end
+end
