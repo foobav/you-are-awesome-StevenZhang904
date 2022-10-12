1ef622231b36a900b895437bc4ffb9d59e934361
foobav <104128111+foobav@users.noreply.github.com>
2022-10-12 21:51:31 +0000

    Typos fixed

--- test/study_reminder/accounts_test.exs
+++ test/study_reminder/accounts_test.exs
@@ -26,4 +26,5 @@ defmodule StudyReminder.AccountsTest do
 
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
-      asse
+      assert user.email == "user@business.com"
+   
