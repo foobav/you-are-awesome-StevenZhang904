5d73ad7f83eb5a53f17bbde3537ae8e5f357af7b
foobav <104128111+foobav@users.noreply.github.com>
2022-10-20 11:04:06 +0000

    Setting version to 0.10.0-SNAPSHOT

--- test/study_reminder/accounts_test.exs
+++ test/study_reminder/accounts_test.exs
@@ -36,4 +36,5 @@ defmodule StudyReminder.AccountsTest do
 
     test "update_user/2 with valid data updates the user" do
       user = user_fixture()
-      assert {:ok, user} = Accounts
+      assert {:ok, user} = Accounts.update_user(user, @update_attrs)
+      assert %Us
