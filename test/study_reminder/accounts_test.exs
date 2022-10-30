89ce815b4191d531c167e36f5f83d07b454b0607
foobav <104128111+foobav@users.noreply.github.com>
2022-10-30 06:50:36 +0000

    Added more icons.

--- test/study_reminder/accounts_test.exs
+++ test/study_reminder/accounts_test.exs
@@ -45,4 +45,4 @@ defmodule StudyReminder.AccountsTest do
     test "update_user/2 with invalid data returns error changeset" do
       user = user_fixture()
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
-      assert user.e
+      assert user.email == Accounts.get_user!(user.id).email
