b8c390d80aa29a00ba03005b8b9ca1a42a29e43c
foobav <104128111+foobav@users.noreply.github.com>
2022-12-01 20:48:45 +0000

    sum

--- test/study_reminder_web/views/error_view_test.exs
+++ test/study_reminder_web/views/error_view_test.exs
@@ -1,4 +1,7 @@
 defmodule StudyReminderWeb.ErrorViewTest do
   use StudyReminderWeb.ConnCase, async: true
 
-  # Bring render/3 and render_to_string/3 for t
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test 
