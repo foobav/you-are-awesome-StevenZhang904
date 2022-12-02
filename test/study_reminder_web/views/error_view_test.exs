95839f3b13f5b408837bf5c221e1e91fd4ffc9e4
foobav <104128111+foobav@users.noreply.github.com>
2022-12-02 16:29:45 +0000

    2.1.11

--- test/study_reminder_web/views/error_view_test.exs
+++ test/study_reminder_web/views/error_view_test.exs
@@ -4,4 +4,5 @@ defmodule StudyReminderWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test 
+  test "renders 404.json" do
+    assert render
