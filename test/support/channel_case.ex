5ff26bd693054a6a7f9bfa0170ca95592a3774ca
foobav <104128111+foobav@users.noreply.github.com>
2022-12-13 15:58:31 +0000

    Remove unneeded includes

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -1,3 +1,6 @@
 defmodule StudyReminderWeb.ChannelCase do
   @moduledoc """
-  This module defines the test ca
+  This module defines the test case to be used by
+  channel tests.
+
+  Such tests rely
