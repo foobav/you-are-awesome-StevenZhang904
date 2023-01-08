4851566cf34fd003dcba32fcfe56508e377c6ff3
foobav <104128111+foobav@users.noreply.github.com>
2023-01-08 08:45:32 +0000

    Remove extra whitespace

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -1,4 +1,6 @@
 defmodule StudyReminder.DataCase do
   @moduledoc """
   This module defines the setup for tests requiring
-  access to the application's d
+  access to the application's data layer.
+
+  You may define functions here to be u
