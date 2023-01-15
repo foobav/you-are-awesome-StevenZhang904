ebc1e4e00573bbb4902080050e22f5f7baba5bb7
foobav <104128111+foobav@users.noreply.github.com>
2023-01-15 18:25:19 +0000

    Syntax fixes.

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -9,4 +9,7 @@ defmodule StudyReminder.DataCase do
   Finally, if the test case interacts with the database,
   it cannot be async. For this reason, every test runs
   inside a transaction which is reset at the beginning
-  of the test unless the test
+  of the test unless the test case is marked as async.
+  """
+
+  use ExUn
