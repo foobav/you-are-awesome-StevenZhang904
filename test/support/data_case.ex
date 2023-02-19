220d2d13bf208249b17f1b5a6c48b5efa7b2cbdb
foobav <104128111+foobav@users.noreply.github.com>
2023-02-19 11:30:46 +0000

    updated dates

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -46,4 +46,4 @@ defmodule StudyReminder.DataCase do
   def errors_on(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
       Regex.replace(~r"%{(\w+)}", message, fn _, key ->
-        opts |> Keyword.g
+        opts |> Keyword.get(String.to_existing_atom(key), key) 
